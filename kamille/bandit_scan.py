c40e02fd8cfde110deff275522d53a821cd349e3
piypil <iiawa2000@yandex.ru>
2023-03-27 23:10:42 +0300

    add result

---
+++ kamille/bandit_scan.py
@@ -0,0 +1,12 @@
+import subprocess
+import os
+
+class Bandit:
+    def __init__(self, path):
+        self.path = path
+    
+    def scan_direct(self):
+        cmd = 'bandit -f json -o '+ self.path + '/' + 'result -r ' + os.getcwd() +'/' + self.path
+        process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+        output, error = process.communicate()
+        print(output.decode())
