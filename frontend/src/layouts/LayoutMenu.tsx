5582d671321ed65604479f54fab3be6359f2a318
piypil <iiawa2000@yandex.ru>
2023-07-04 23:09:19 +0300

    Layout

---
+++ frontend/src/layouts/LayoutMenu.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Layout, Menu, Button, theme } from 'antd';
+
+const { Header, Sider, Content } = Layout;
+
+interface LayoutMenuProps {
+  children: React.ReactNode;
+}
+
+export default function LayoutMenu({ children }: LayoutMenuProps) {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  return (
+    <Layout style={{minHeight:'100vh'}}>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          items={[
+            {
+              key: '1',
+              icon: <UserOutlined />,
+              label: 'nav 1',
+            },
+            {
+              key: '2',
+              icon: <VideoCameraOutlined />,
+              label: 'nav 2',
+            },
+            {
+              key: '3',
+              icon: <UploadOutlined />,
+              label: 'nav 3',
+            },
+          ]}
+        />
+      </Sider>
+      <Layout className="site-layout" style={{maxHeight:'100vh', overflow:'auto'}}>
+        <Header style={{ padding: 0, background: colorBgContainer }}>
+          {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+            className: 'trigger',
+            onClick: () => setCollapsed(!collapsed),
+          })}
+        </Header>
+        <Content
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+          }}
+        >
+          {children}
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
--- frontend/src/pages/WelcomePage.tsx
+++ frontend/src/pages/WelcomePage.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Col, Row } from 'antd';
 import { UploadFile } from '../components/UploadFile';
+import LayoutMenu from '../layouts/LayoutMenu'
 
 export function WelcomePage() {
   return (
     <div>
       <Row>
         <Col span={8}>
+        <LayoutMenu>
           <UploadFile />
+        </LayoutMenu>
         </Col>
       </Row>
     </div>
