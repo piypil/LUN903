b44305c53f23235dc863639eee1564966acded3b
piypil <iiawa2000@yandex.ru>
2023-07-15 09:30:05 +0400

    ProjectResultsPage

--- frontend/src/pages/ProjectResultsPage.tsx
+++ frontend/src/pages/ProjectResultsPage.tsx
@@ -1,56 +1,89 @@
 import React, { useEffect, useState } from 'react';
-import { Result, Table } from 'antd';
+import { Table, Typography } from 'antd';
+import axios from 'axios';
 import { useParams } from 'react-router-dom';
 
-interface Result {
-  file_id: number
-  result_data: {
-    errors: []
-    generated_at: string
-    metrics: {}
-    results:
-        {
-            code: string
-            col_offset: number
-            end_col_offset: number
-            filename: string
-            issue_confidence : string
-            issue_cwe: {
-                id: number
-                link: string
-            }
-            issue_severity: string
-            issue_text: string
-            line_number: number
-            line_range: number[]
-            more_info: string
-            test_id: string
-            test_name: string
-        }[]
-  }
+const { Title } = Typography;
+
+interface Vulnerability {
+  code: string;
+  filename: string;
+  test_name: string;
+  issue_text: string;
+  line_number: number;
+  end_col_offset: number;
 }
 
 const ProjectResultsPage: React.FC = () => {
-  const { projectId } = useParams();
-  const [results, setResults] = useState<Result[]>([]);
+  const { projectId } = useParams<{ projectId: string }>();
+  const [vulnerabilities, setVulnerabilities] = useState<Vulnerability[]>([]);
+  const [selectedVulnerability, setSelectedVulnerability] = useState<Vulnerability | null>(null);
+  const [code, setCode] = useState<string>('');
 
   useEffect(() => {
-    fetch(`http://localhost:8000/api/results/${projectId}/`)
-      .then(response => response.json())
-      .then(data => setResults(data))
-      .catch(error => console.error(error));
+    axios
+      .get(`http://localhost:8000/api/results/${projectId}/`)
+      .then((response) => {
+        const data = response.data;
+        setVulnerabilities(data[0]?.result_data?.results || []);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }, [projectId]);
 
   const columns = [
-    { title: 'Code', dataIndex: 'code', key: 'code' },
-    { title: 'Test Name', dataIndex: 'test_name', key: 'test_name' },
-    { title: 'Test Id', dataIndex: 'test_id', key: 'test_id' }
+    {
+      title: 'Vulnerability',
+      dataIndex: 'test_name',
+      key: 'test_name',
+    },
+    {
+      title: 'Description',
+      dataIndex: 'issue_text',
+      key: 'issue_text',
+    },
+    {
+      title: 'Code',
+      dataIndex: 'filename',
+      key: 'code',
+      render: (text: string, vulnerability: Vulnerability) => (
+        <div className="code-container">
+          {selectedVulnerability === vulnerability && (
+            <pre>{code}</pre>
+          )}
+        </div>
+      ),
+    },
   ];
+
+  const handleRowClick = (record: Vulnerability) => {
+    setSelectedVulnerability(record);
+    fetchCode(record.filename);
+  };
+
+  const fetchCode = (filePath: string) => {
+    axios
+      .get(`http://localhost:8000/api/code/?file_path=${filePath}`)
+      .then((response) => {
+        const data = response.data;
+        setCode(data.code);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
   return (
     <div>
-      <h1>Результаты проекта {projectId}</h1>
-      <Table dataSource={results} columns={columns} />
+      <Title level={2}>Результаты проекта {projectId}</Title>
+      <Table
+        dataSource={vulnerabilities}
+        columns={columns}
+        onRow={(record) => ({
+          onClick: () => handleRowClick(record),
+        })}
+      />
     </div>
   );
 };
