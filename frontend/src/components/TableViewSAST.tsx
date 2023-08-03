0ce01bb5ddc65ff8889130b0d3a9cfc36487a68f
piypil <iiawa2000@yandex.ru>
2023-08-04 01:28:59 +0300

    review

--- frontend/src/components/TableViewSAST.tsx
+++ frontend/src/components/TableViewSAST.tsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { Space, Table } from 'antd';
+import { Space, Table, Spin, Modal, Button } from 'antd';
+import { ExclamationCircleOutlined } from '@ant-design/icons';
 import type { ColumnsType } from 'antd/es/table';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
+
+const { confirm } = Modal;
 
 const API_BASE_URL = process.env.REACT_APP_API_URL;
 
@@ -15,6 +18,27 @@ interface FileData {
 }
 
 export function TableViewSAST() {
+  const [data, setData] = useState<FileData[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const deleteRecord = (id: number) => {
+    // надо добавить удаление записи
+    console.log('Delete record with id:', id);
+  }
+
+  const showDeleteConfirm = (id: number) => {
+    confirm({
+      title: 'Are you sure delete this record?',
+      icon: <ExclamationCircleOutlined />,
+      onOk() {
+        deleteRecord(id);
+      },
+      onCancel() {
+        console.log('Cancel');
+      },
+    });
+  }
+
   const columns: ColumnsType<FileData> = [
     {
       title: 'Project',
@@ -45,32 +69,39 @@ export function TableViewSAST() {
           <a href={record.file} target="_blank" rel="noopener noreferrer">
             Download
           </a>
-          <a>Delete</a>
+          <Button danger onClick={() => showDeleteConfirm(record.id)}>
+            Delete
+          </Button>
         </Space>
       ),
     },
   ];
-
-  const [data, setData] = useState<FileData[]>([]);
 
   useEffect(() => {
     axios
       .get(`${API_BASE_URL}/files/`)
       .then((response) => {
         setData(response.data);
+        setLoading(false);
       })
       .catch((error) => {
         console.log(error);
+        setLoading(false);
       });
   }, []);
+
+  if (loading) {
+    return <Spin size="large" />;
+  }
 
   return (
     <Table
       columns={columns}
       dataSource={data}
-      loading={data.length === 0}
+      rowKey={(record) => record.id.toString()}
     />
   );
 }
 
 export default TableViewSAST;
+
