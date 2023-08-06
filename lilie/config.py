692c502a07478640bc5ba4642eb9f762558ebe02
piypil <iiawa2000@yandex.ru>
2023-08-06 18:01:54 +0300

    config

--- lilie/config.py
+++ lilie/config.py
@@ -13,3 +13,7 @@ DB_NAME = str(os.getenv("DB_NAME"))
 ZAP_KEY = str(os.getenv("ZAP_KEY"))
 ZAP_PORT = str(os.getenv("ZAP_PORT"))
 ZAP_HOST = str(os.getenv("ZAP_HOST"))
+
+DJANGO_SECRET_KEY = str(os.getenv("DJANGO_SECRET_KEY"))
+DJANGO_DEBUG = bool(os.getenv("DJANGO_DEBUG"))
+DJANGO_ALLOWED_HOSTS = str(os.getenv("DJANGO_ALLOWED_HOSTS"))
