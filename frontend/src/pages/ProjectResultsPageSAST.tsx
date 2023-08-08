8bbe23bb860f704a39a28a5690299b5a3f797cf5
piypil <iiawa2000@yandex.ru>
2023-08-09 01:12:33 +0300

    SAST SAS SAS

--- frontend/src/pages/ProjectResultsPageSAST.tsx
+++ frontend/src/pages/ProjectResultsPageSAST.tsx
@@ -7,9 +7,11 @@ import { EditorView } from '@codemirror/view';
 import { classname } from '@uiw/codemirror-extensions-classname';
 import { langs } from '@uiw/codemirror-extensions-langs';
 import LayoutMenu from '../layouts/LayoutMenu';
+import { Menu } from 'antd';
+import { AppstoreOutlined } from '@ant-design/icons';
+import { Card } from 'antd';
 
 const { Title } = Typography;
-const { TabPane } = Tabs;
 const API_BASE_URL = process.env.REACT_APP_API_URL;
 
 function getColor(priority: string) {
@@ -46,8 +48,9 @@ const ProjectResultsPageSAST: React.FC = () => {
   const [vulnerabilities, setVulnerabilities] = useState<Vulnerability[]>([]);
   const [selectedVulnerability, setSelectedVulnerability] = useState<Vulnerability | null>(null);
   const [code, setCode] = useState<string>('');
-  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
-
+  const showVulnerabilityDetails = (vulnerability: Vulnerability) => {
+    setSelectedVulnerability(vulnerability);
+  };
   const codeContainerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -119,57 +122,46 @@ const ProjectResultsPageSAST: React.FC = () => {
             </div>
           </Col>
           <Col span={8}>
-            <Tabs
-              tabPosition="left"
-              style={{ height: '100vh', overflowY: 'auto' }}
-              activeKey={selectedCategory || 'default'}
-              onChange={(key) => setSelectedCategory(key)}
-            >
+            <Menu mode="inline" theme="dark" style={{ height: '50vh', overflowY: 'auto' }} defaultOpenKeys={categories}>
               {categories.map((category) => (
-                <TabPane tab={category} key={category}>
-                  <div>
-                    {vulnerabilities
-                      .filter((vuln) => vuln.test_name === category)
-                      .map((vuln) => (
-                        <div
-                          key={vuln.test_name}
-                          className={`vulnerability-block ${
-                            selectedVulnerability === vuln ? 'selected' : ''
-                          }`}
-                          onClick={() => handleRowClick(vuln)}
-                        >
-                          <h3>{vuln.test_name}</h3>
-                          <p>{vuln.issue_text}</p>
-                          {selectedVulnerability === vuln && (
-                            <Descriptions bordered column={1}>
-                              <Descriptions.Item label="CWE">
-                                <a href={vuln.issue_cwe.link} target="_blank" rel="noopener noreferrer">
-                                  {vuln.issue_cwe.id}
-                                </a>
-                              </Descriptions.Item>
-                              <Descriptions.Item label="Severity">
-                                <Tag color={getColor(vuln.issue_severity)}>
-                                  {vuln.issue_severity}
-                                </Tag>
-                              </Descriptions.Item>
-                              <Descriptions.Item label="Confidence">
-                                <Tag color={getColor(vuln.issue_confidence)}>
-                                  {vuln.issue_confidence}
-                                </Tag>
-                              </Descriptions.Item>
-                            </Descriptions>
-                          )}
-                        </div>
-                      ))}
-                  </div>
-                </TabPane>
+                <Menu.SubMenu key={category} title={category} icon={<AppstoreOutlined />}>
+                  {vulnerabilities
+                    .filter((vuln) => vuln.test_name === category)
+                    .map((vuln) => (
+                      <Menu.Item onClick={() => handleRowClick(vuln)}>
+                        {vuln.filename}
+                      </Menu.Item>
+                    ))}
+                </Menu.SubMenu>
               ))}
-            </Tabs>
+            </Menu>
+            {selectedVulnerability && (
+                <Card title={selectedVulnerability.test_name} style={{ width: '100%', marginTop: 10 }}>
+                  <p>{selectedVulnerability.issue_text}</p>
+                  <Descriptions bordered column={1}>
+                    <Descriptions.Item label="CWE">
+                      <a href={selectedVulnerability.issue_cwe.link} target="_blank" rel="noopener noreferrer">
+                        {selectedVulnerability.issue_cwe.id}
+                      </a>
+                    </Descriptions.Item>
+                    <Descriptions.Item label="Severity">
+                      <Tag color={getColor(selectedVulnerability.issue_severity)}>
+                        {selectedVulnerability.issue_severity}
+                      </Tag>
+                    </Descriptions.Item>
+                    <Descriptions.Item label="Confidence">
+                      <Tag color={getColor(selectedVulnerability.issue_confidence)}>
+                        {selectedVulnerability.issue_confidence}
+                      </Tag>
+                    </Descriptions.Item>
+                  </Descriptions>
+                </Card>
+              )}
           </Col>
         </Row>
       </LayoutMenu>
     </div>
-  );
+  );  
 };
 
 export default ProjectResultsPageSAST;
