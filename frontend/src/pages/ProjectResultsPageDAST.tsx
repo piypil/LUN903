e9ee7287d382b75ba06621a5a0cdd6d7b4a63e28
piypil <iiawa2000@yandex.ru>
2023-11-03 14:40:58 +0300

    interface redesign

--- frontend/src/pages/ProjectResultsPageDAST.tsx
+++ frontend/src/pages/ProjectResultsPageDAST.tsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { Typography, Card, Row, Col, Badge, Descriptions, Button, Collapse, List, Space, Tabs } from 'antd';
+import { Typography, Card, Row, Col, Descriptions, Divider, Space, Tabs } from 'antd';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import LayoutMenu from '../layouts/LayoutMenu';
 import { useTheme } from '../components/ThemeContext';
-import { ArrowLeftOutlined } from '@ant-design/icons';
+import { Menu } from 'antd';
+import { BugOutlined } from '@ant-design/icons';
+import CodeMirror from '@uiw/react-codemirror';
 
-const { Panel } = Collapse;
 const { TabPane } = Tabs;
-
+const { SubMenu } = Menu;
 
 const API_BASE_URL = process.env.REACT_APP_API_URL;
 
@@ -119,6 +120,7 @@ const ProjectResultsPageDAST: React.FC = () => {
     }, {} as Record<string, number>);
   };
 
+
   const groupByRuleDescription = (results: ReportResult[]): Record<string, ReportResult[]> => {
     return results.reduce<Record<string, ReportResult[]>>((acc, result) => {
       const rule = data?.results.runs[0].tool.driver.rules.find(r => r.id === result.ruleId);
@@ -146,11 +148,6 @@ const ProjectResultsPageDAST: React.FC = () => {
     setViewDetails(true);
   };
 
-  const handleBackClick = () => {
-    setViewDetails(false);
-    setCurrentResult(null);
-  };
-
   const handleConfidenceFilter = (confidence: string | null) => {
     setFilterConfidence(confidence);
   };
@@ -173,81 +170,92 @@ const ProjectResultsPageDAST: React.FC = () => {
   return (
     <div className={theme === 'dark' ? 'dark-theme' : ''}>
       <LayoutMenu>
-        {!viewDetails ? (
-          <>
-            <Typography.Title level={2}>Test</Typography.Title>
-            <Typography.Title level={3}>Общий обзор</Typography.Title>
-            <Row gutter={[16, 16]}>
-              {Object.entries(confidenceCounts).map(([confidence, count]) => (
-                <Col span={6} key={confidence}>
-                  <Card
-                    hoverable
-                    style={{ borderColor: getConfidenceColor(confidence), borderWidth: 2 }}
-                    onClick={() => handleConfidenceFilter(confidence)}
-                    title={confidence.charAt(0).toUpperCase() + confidence.slice(1)}
-                    headStyle={{ backgroundColor: getConfidenceColor(confidence, 0.1), color: getConfidenceColor(confidence), fontWeight: 'bold' }}
-                  >
-                    <Typography.Text style={{ fontSize: 24, fontWeight: 'bold' }}>{count}</Typography.Text>
-                  </Card>
-                </Col>
-              ))}
-            </Row>
-            <Typography.Title level={3}>Report Results:</Typography.Title>
-            <Collapse>
-              {Object.entries(groupedResults).filter(([_, results]) => {
-                if (!filterConfidence) return true;
-                const rule = data?.results.runs[0].tool.driver.rules.find(r => r.id === results[0].ruleId);
-                return rule?.properties?.confidence === filterConfidence;
-              }).map(([ruleDescription, results]) => {
-                const rule = data?.results.runs[0].tool.driver.rules.find(r => r.id === results[0].ruleId);
-                const confidence = rule?.properties?.confidence;
+        <Typography.Title level={2}>Test</Typography.Title>
+        
+        <div>
+          <Row gutter={[0, 12]}>
+            {Object.entries(confidenceCounts).map(([confidence, count]) => (
+              <Col span={2} key={confidence}>
+                <Card
+                  hoverable
+                  style={{
+                    borderColor: getConfidenceColor(confidence),
+                    borderWidth: 1,
+                    width: '100%',
+                    borderTop: `2px solid ${getConfidenceColor(confidence)}`,
+                    backgroundColor: '#ffffff',
+                  }}
+                  onClick={() => handleConfidenceFilter(confidence)}
+                  title={<span style={{ fontSize: '14px' }}>{confidence.charAt(0).toUpperCase() + confidence.slice(1)}</span>}
+                  headStyle={{
+                    fontWeight: 'bold',
+                    borderBottom: '1px solid #d9d9d9',
+                  }}
+                  bodyStyle={{
+                    padding: '10px',
+                  }}
+                >
+                  <Typography.Text style={{ fontSize: '12px', fontWeight: 'bold', lineHeight: '14px' }}>{count}</Typography.Text>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </div>
 
-                return (
-                  <Panel header={
-                    <div>
-                      <Typography.Text strong>{ruleDescription}</Typography.Text>
-                      {confidence && (
-                        <Badge
-                          style={{ marginLeft: 16 }}
-                          color={getConfidenceColor(confidence)}
-                          text={confidence.charAt(0).toUpperCase() + confidence.slice(1)}
-                        />
-                      )}
-                    </div>
-                  } key={ruleDescription}>
-                    <List
-                      dataSource={results}
-                      renderItem={result => {
+        <Row>
+          <Col span={12}>
+            <Typography.Title level={3}>Results:</Typography.Title>
+            <Col span={14}>
+              <Menu
+                style={{ height: '100%', overflow: 'auto' }}
+              >
+                {Object.entries(groupedResults).filter(([_, results]) => {
+                  if (!filterConfidence) return true;
+                  const rule = data?.results.runs[0].tool.driver.rules.find(r => r.id === results[0].ruleId);
+                  return rule?.properties?.confidence === filterConfidence;
+                }).map(([ruleDescription, results]) => {
+                  const rule = data?.results.runs[0].tool.driver.rules.find(r => r.id === results[0].ruleId);
+                  const confidence = rule?.properties?.confidence;
+
+                  return (
+                    <SubMenu
+                      key={ruleDescription}
+                      title={
+                        <span>
+                          {confidence && (
+                            <>
+                              <Divider type="vertical" style={{ borderLeft: `3px solid ${getConfidenceColor(confidence)}`, height: '100%', marginLeft: 16, marginRight: 16 }} />
+                            </>
+                          )}
+                          <BugOutlined />
+                          <Typography.Text strong style={{ marginLeft: 8 }}>{ruleDescription}</Typography.Text>
+                        </span>
+                      }
+                    >
+                      {results.map(result => {
                         const artifactUri = result.locations?.[0]?.physicalLocation.artifactLocation?.uri;
                         return (
-                          <List.Item onClick={() => handleResultClick(result)}>
-                            <List.Item.Meta
-                              description={artifactUri}
-                            />
-                          </List.Item>
+                          <Menu.Item key={result.ruleId} onClick={() => handleResultClick(result)}>
+                            {artifactUri}
+                          </Menu.Item>
                         );
-                      }}
-                    />
-                  </Panel>
-                );
-              })}
-            </Collapse>
-          </>
-        ) : (
-          <>
-            <Space direction="vertical" style={{ padding: '20px', width: '100%' }}>
+                      })}
+                    </SubMenu>
+                  );
+                })}
+              </Menu>
+            </Col>
+          </Col>
 
-              <Button type="primary" onClick={handleBackClick} icon={<ArrowLeftOutlined />}>
-                Назад
-              </Button>
-
+          <Col span={12}>
+            <Space direction="vertical">
               <Typography.Title level={2}>
                 {currentResult && data?.results.runs[0].tool.driver.rules.find(r => r.id === currentResult.ruleId)?.shortDescription.text}
               </Typography.Title>
 
               <Row gutter={[16, 16]}>
                 <Col span={24}>
-                  <Descriptions title="Artifact Location" bordered>
+                  <Descriptions title="URL" bordered>
                     <Descriptions.Item>
                       <Typography.Text strong>{currentResult?.locations[0]?.physicalLocation.artifactLocation.uri}</Typography.Text>
                     </Descriptions.Item>
@@ -257,47 +265,90 @@ const ProjectResultsPageDAST: React.FC = () => {
 
               <Row gutter={[16, 16]}>
                 <Col span={24}>
-                  <Typography.Title level={4}>Level: {currentResult?.level}</Typography.Title>
                   <Typography.Paragraph>
-                    <Typography.Text strong>Message:</Typography.Text> {currentResult?.message.text}
+                    <Typography.Text strong>Description:</Typography.Text> {currentResult && data?.results.runs[0].tool.driver.rules.find(r => r.id === currentResult.ruleId)?.fullDescription.text}
                   </Typography.Paragraph>
                 </Col>
               </Row>
 
+              <Row gutter={[16, 16]}>
+                <Col span={24}>
+                  <Typography.Paragraph>
+                    <Typography.Text strong>Other Info:</Typography.Text> {currentResult?.message.text}
+                  </Typography.Paragraph>
+                </Col>
+              </Row>
+
+              <Row gutter={[16, 16]}>
+                <Col span={24}>
+                  <Typography.Paragraph>
+                    <Typography.Text strong>Solution:</Typography.Text> {currentResult && data?.results.runs[0].tool.driver.rules.find(r => r.id === currentResult.ruleId)?.properties.solution?.text}
+                  </Typography.Paragraph>
+                </Col>
+              </Row>
+
+
               <div style={{ padding: '20px' }}>
                 <Tabs defaultActiveKey="request" type="card">
+
                   <TabPane tab="Web Request" key="request">
                     <Descriptions bordered column={1}>
                       <Descriptions.Item label="Method">
                         <Typography.Text strong>{currentResult?.webRequest.method}</Typography.Text>
                       </Descriptions.Item>
-                      <Descriptions.Item label="URI">
-                        <Typography.Text strong>{currentResult?.webRequest.target}</Typography.Text>
-                      </Descriptions.Item>
                       <Descriptions.Item label="Headers">
-                        <pre>{JSON.stringify(currentResult?.webRequest.headers, null, 2)}</pre>
+                        <CodeMirror
+                          value={JSON.stringify(currentResult?.webRequest.headers, null, 2)}
+                        />
                       </Descriptions.Item>
                     </Descriptions>
                   </TabPane>
+
+                  <TabPane tab="Web Request Body" key="requestBody">
+                    <Descriptions bordered column={1}>
+                      <Descriptions.Item label="Method">
+                        <Typography.Text strong>{currentResult?.webRequest.method}</Typography.Text>
+                      </Descriptions.Item>
+                      <Descriptions.Item label="Headers">
+                        <CodeMirror
+                          value={JSON.stringify(currentResult?.webRequest.body, null, 2)}
+                        />
+                      </Descriptions.Item>
+                    </Descriptions>
+                  </TabPane>
+
                   <TabPane tab="Web Response" key="response">
                     <Descriptions bordered column={1}>
                       <Descriptions.Item label="Status Code">
                         <Typography.Text strong>{currentResult?.webResponse.statusCode}</Typography.Text>
                       </Descriptions.Item>
-                      <Descriptions.Item label="Reason">
-                        <Typography.Text strong>{currentResult?.webResponse.reasonPhrase}</Typography.Text>
-                      </Descriptions.Item>
                       <Descriptions.Item label="Headers">
-                        <pre>{JSON.stringify(currentResult?.webResponse.headers, null, 2)}</pre>
+                        <CodeMirror
+                          value={JSON.stringify(currentResult?.webResponse.headers, null, 2)}
+                        />
                       </Descriptions.Item>
                     </Descriptions>
                   </TabPane>
+
+                  <TabPane tab="Web Response Body" key="responseBody">
+                    <Descriptions bordered column={1}>
+                      <Descriptions.Item label="Status Code">
+                        <Typography.Text strong>{currentResult?.webResponse.statusCode}</Typography.Text>
+                      </Descriptions.Item>
+                      <Descriptions.Item label="Headers">
+                        <CodeMirror
+                          value={JSON.stringify(currentResult?.webResponse.body, null, 2)}
+                        />
+                      </Descriptions.Item>
+                    </Descriptions>
+                  </TabPane>
+
                 </Tabs>
               </div>
-              
             </Space>
-          </>
-        )}
+          </Col>
+
+        </Row>
       </LayoutMenu>
     </div>
   );
